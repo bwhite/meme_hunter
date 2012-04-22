628c9404e5e3dd1e409594ffe6e9ff8443147649
Brandyn A. White <bwhite@dappervision.com>
2012-04-22 16:37:20 -0400

    Basic extension
    Signed-off-by: Brandyn A. White <bwhite@dappervision.com>

---
+++ chrome/manifest.js
@@ -0,0 +1,12 @@
+{
+    "name": "Meme Hunter",
+    "version": ".01",
+    "manifest_version": 2,
+    "description": "Hunts memes",
+    "browser_action": {
+        "default_icon": "icon.png"
+    },
+    "permissions": [
+            "http://api.flickr.com/"
+          ]
+}
